aa79223076758f87afea518ed2f2f781183a30c3
Ko'palov Muhammadjon <kupalovv.muhammadjon@gmail.com>
2024-05-31 11:38:09 +0500

    CRUD done

--- lesson29/main.go
+++ lesson29/main.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
+	// ketma ket komentdan chiqalib ishlaturasiz bo'lmasa array bo'shab qolib error berishi mumkin
 	u := postgres.NewUserRepo(db)
 	// users := generator.GenerateRandomUser()
 	// for i := 0; i < 30; i++ {
@@ -26,17 +26,17 @@ func main() {
 	}
 	printer.PrintUsers(users)
 
-	user, err := u.GetById((*users)[0].ID)
-	if err != nil {
-		log.Println("Cannot get users by id")
-	}
-	printer.PrintUser(*user)
+	// user, err := u.GetById((*users)[0].ID)
+	// if err != nil {
+	// 	log.Println("Cannot get users by id")
+	// }
+	// printer.PrintUser(*user)
 
-	users, err = u.GetByFirstName("Alice")
-	if err != nil {
-		log.Println("Cannot get users by first_name")
-	}
-	printer.PrintUsers(users)
+	// users, err = u.GetByFirstName("Alice")
+	// if err != nil {
+	// 	log.Println("Cannot get users by first_name")
+	// }
+	// printer.PrintUsers(users)
 
 	(*users)[0].FirstName = "Chello"
 	err = u.UpdateUser((*users)[0])
@@ -45,8 +45,8 @@ func main() {
 	}
 	printer.PrintUser((*users)[0])
 
-	err = u.DeleteUser((*users)[0].ID)
-	if err != nil {
-		log.Println("Cannot delete user")
-	}
+	// err = u.DeleteUser((*users)[0].ID)
+	// if err != nil {
+	// 	log.Println("Cannot delete user")
+	// }
 }
