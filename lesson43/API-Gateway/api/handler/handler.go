408f32185270cc6e038da5c010e2a8788b2b4729
Ko'palov Muhammadjon <kupalovv.muhammadjon@gmail.com>
2024-06-23 21:54:57 +0500

    Project is ready

--- lesson43/API-Gateway/api/handler/handler.go
+++ lesson43/API-Gateway/api/handler/handler.go
@@ -16,9 +16,45 @@ func NewHandler() *Handler{
 	return &Handler{&http.Client{}}
 }
 
-func (h *Handler) Handle(c *gin.Context) {
+func (h *Handler) HandleUsers(c *gin.Context) {
 
 	url := "http://localhost:8888" + c.Request.URL.String()
+
+	req, err := http.NewRequest(c.Request.Method, url, c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error":   "StatusBadRequest",
+			"Message": err,
+		})
+		log.Println("Error while making rerquest ", err)
+		return
+	}
+	resp, err := h.Client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error":   "StatusBadRequest",
+			"Message": err,
+		})
+		log.Println("Error while getting rerquest ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error":   "StatusBadRequest",
+			"Message": err,
+		})
+		log.Println("Bad request ", err)
+		return
+	}
+	c.JSON(200, string(body))
+}
+
+func (h *Handler) HandleBilling(c *gin.Context) {
+
+	url := "http://localhost:9999" + c.Request.URL.String()
 
 	req, err := http.NewRequest(c.Request.Method, url, c.Request.Body)
 	if err != nil {
